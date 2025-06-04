86f1a11dad33b739cbe8c9c8bcaf168ef2c598c0
ABC3Dz <archueyouler@gmail.com>
2025-06-04 11:49:34 +0700

    461

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function init() {
 	document.body.appendChild( container );
     //camera
 	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 100 );
-	camera.position.set(0, 0, 28);
+	camera.position.set(0, 0, 20);
     //scene
 	scene = new THREE.Scene();
 	scene.background = null;
@@ -75,31 +75,40 @@ function init() {
     window.addEventListener( 'resize', onWindowResize );
     
 	// model
-    try {
-         loadGlb(scene, 'models/Abczezeze.glb', {x:0,y:2}, 1.7);
-         loadGlb(scene, 'models/ITCHioObj.glb', {x:-2,y:0}, .8);
-         loadGlb(scene, 'models/GamejoltObj.glb', {x:-.5,y:0});
-         loadGlb(scene, 'models/IndieDBObj.glb', {x:1,y:0});
-         loadGlb(scene, 'models/PlayStoreObj.glb', {x:2.5,y:0},.6);
-         loadGlb(scene, 'models/SketchfabObj.glb', {x:-2,y:-1.5});
-         loadGlb(scene, 'models/SoundcloudObj.glb', {x:-.7,y:-1.5});
-         loadGlb(scene, 'models/GgDriveObj.glb', {x:1,y:-1.5});
-         loadGlb(scene, 'models/YoutubeObj.glb', {x:2.4,y:-1.5});
-         loadGlb(scene, 'models/FbObj.glb', {x:-2,y:-3});
-         loadGlb(scene, 'models/XObj.glb', {x:-.7,y:-3});
-         loadGlb(scene, 'models/MastodonObj.glb', {x:1,y:-3},.6);
-         loadGlb(scene, 'models/BlueskyObj.glb', {x:2.4,y:-3});
-         loadGlb(scene, 'models/VercelObj.glb', {x:-2.2,y:2},1.3).then((gltfScene) => {vercelLoad = gltfScene;});
-         loadGlb(scene, 'models/GithubObj.glb', {x:2,y:3.5});
-         loadGlb(scene, 'models/DeviantartObj.glb', {x:-2,y:-4.8});
-         loadGlb(scene, 'models/TumblrObj.glb', {x:-.8,y:-4.8});
-         loadGlb(scene, 'models/VimeoObj.glb', {x:.4,y:-4.8});
-         loadGlb(scene, 'models/FastworkObj.glb', {x:-2.2,y:3.5},1.3);
-         loadGlb(scene, 'models/RedditObj.glb', {x:2,y:2});
-        // loadGlb(scene, 'models/LinkedinObj.glb', {x:2,y:-4.8});
-    } catch (error) {
-        console.error("Error initializing models:", error);
-    }
+    // try {
+    //      loadGlb(scene, 'models/Abczezeze.glb', {x:0,y:2}, 1.7);
+    //      loadGlb(scene, 'models/ITCHioObj.glb', {x:-2,y:0}, .8);
+    //      loadGlb(scene, 'models/GamejoltObj.glb', {x:-.5,y:0});
+    //      loadGlb(scene, 'models/IndieDBObj.glb', {x:1,y:0});
+    //      loadGlb(scene, 'models/PlayStoreObj.glb', {x:2.5,y:0},.6);
+    //      loadGlb(scene, 'models/SketchfabObj.glb', {x:-2,y:-1.5});
+    //      loadGlb(scene, 'models/SoundcloudObj.glb', {x:-.7,y:-1.5});
+    //      loadGlb(scene, 'models/GgDriveObj.glb', {x:1,y:-1.5});
+    //      loadGlb(scene, 'models/YoutubeObj.glb', {x:2.4,y:-1.5});
+    //      loadGlb(scene, 'models/FbObj.glb', {x:-2,y:-3});
+    //      loadGlb(scene, 'models/XObj.glb', {x:-.7,y:-3});
+    //      loadGlb(scene, 'models/MastodonObj.glb', {x:1,y:-3},.6);
+    //      loadGlb(scene, 'models/BlueskyObj.glb', {x:2.4,y:-3});
+    //      loadGlb(scene, 'models/VercelObj.glb', {x:-2.2,y:2},1.3).then((gltfScene) => {vercelLoad = gltfScene;});
+    //      loadGlb(scene, 'models/GithubObj.glb', {x:2,y:3.5});
+    //      loadGlb(scene, 'models/DeviantartObj.glb', {x:-2,y:-4.8});
+    //      loadGlb(scene, 'models/TumblrObj.glb', {x:-.8,y:-4.8});
+    //      loadGlb(scene, 'models/VimeoObj.glb', {x:.4,y:-4.8});
+    //      loadGlb(scene, 'models/FastworkObj.glb', {x:-2.2,y:3.5},1.3);
+    //      loadGlb(scene, 'models/RedditObj.glb', {x:2,y:2});
+    //     // loadGlb(scene, 'models/LinkedinObj.glb', {x:2,y:-4.8});
+    // } catch (error) {
+    //     console.error("Error initializing models:", error);
+    // }
+    let allIcon;
+    const loaderAllIcon = new GLTFLoader().setPath( 'models/' );
+    loaderAllIcon.load( 'port4rio.glb', async function ( gltf ) {
+        const model = gltf.scene;
+        model.position.set(0,-2, 0);
+        allIcon = gltf.scene;
+        await renderer.compileAsync( model, camera, scene );
+        scene.add( model );
+    } );
 
     //sound
     var listener = new THREE.AudioListener();
@@ -127,6 +136,7 @@ function init() {
     volumeFolder.add( parameters, 'Audio2' ).min( 0.0 ).max( 1.0 ).step( 0.01 ).onChange( function () {
                         sound2.setVolume( parameters.Audio2 );
     } );
+    volumeFolder.close();
     var positionLight = gui.addFolder('Light Position');
     positionLight.add( parameters, 'X' ).min( -1.0 ).max( 1.0 ).step( 0.01 ).onChange( function () {
         dirLight.position.x = parameters.X;
@@ -137,50 +147,51 @@ function init() {
     positionLight.add( parameters, 'Z' ).min( -1.0 ).max( 1.0 ).step( 0.01 ).onChange( function () {
         dirLight.position.z = parameters.Z;
     } );
+    //positionLight.close();
 
     //game stuff
-    gameStuff('./img/Tek1-min.png',3,1.7,"Tek1",-5,2+2).then((mesh) => {
-        tek1Mesh = mesh;
-        scene.add(tek1Mesh);
-    });
-    gameStuff('./img/Tek2-min.png',3,1.7,'Tek2',-5,0+2).then((mesh) => {
-        tek2Mesh = mesh;
-        scene.add(tek2Mesh);
-    });
-    gameStuff('./img/Tek3-min.png',3,1.7,'Tek3',-5,-2+2).then((mesh) => {
-        tek3Mesh = mesh
-        scene.add(tek3Mesh);
-    });
-    gameStuff('./img/Tek4-min.png',2.5,1.7,'Tek4',-5,-4+2).then((mesh) => {
-        tek4Mesh = mesh;
-        scene.add(tek4Mesh);
-    });
-    gameStuff('./img/Tek5-min.png',3,1.7,'Tek5',-5,-6+2).then((mesh) => {
-        tek5Mesh = mesh;
-        scene.add(tek5Mesh);
-    });
+    // gameStuff('./img/Tek1-min.png',3,1.7,"Tek1",-5,2+2).then((mesh) => {
+    //     tek1Mesh = mesh;
+    //     scene.add(tek1Mesh);
+    // });
+    // gameStuff('./img/Tek2-min.png',3,1.7,'Tek2',-5,0+2).then((mesh) => {
+    //     tek2Mesh = mesh;
+    //     scene.add(tek2Mesh);
+    // });
+    // gameStuff('./img/Tek3-min.png',3,1.7,'Tek3',-5,-2+2).then((mesh) => {
+    //     tek3Mesh = mesh
+    //     scene.add(tek3Mesh);
+    // });
+    // gameStuff('./img/Tek4-min.png',2.5,1.7,'Tek4',-5,-4+2).then((mesh) => {
+    //     tek4Mesh = mesh;
+    //     scene.add(tek4Mesh);
+    // });
+    // gameStuff('./img/Tek5-min.png',3,1.7,'Tek5',-5,-6+2).then((mesh) => {
+    //     tek5Mesh = mesh;
+    //     scene.add(tek5Mesh);
+    // });
     
 
-    gameStuff('./img/MomGame-min.png',3,1.7,'MomGame',5,-6+2).then((mesh) => {
-        MomGame = mesh;
-        scene.add(MomGame);
-    });
-    gameStuff('./img/Lgg-min.png',3,1.7,'Lgg',5,-4+2).then((mesh) => {
-        Lgg = mesh;
-        scene.add(Lgg);
-    });
-    gameStuff('./img/ClickNaja-min.png',3,1.7,'ClickNaja',5,-2+2).then((mesh) => {
-        ClickNaja = mesh;
-        scene.add(ClickNaja);
-    });
-    gameStuff('./img/GeometricBowling-min.png',3,1.7,'GeometricBowling',5,0+2).then((mesh) => {
-        GeometricBowling = mesh;
-        scene.add(GeometricBowling);
-    });
-    gameStuff('./img/Yingleak-min.png',3,1.7,'Yingleak',5,2+2).then((mesh) => {
-        YingLeak = mesh;
-        scene.add(YingLeak);
-    });
+    // gameStuff('./img/MomGame-min.png',3,1.7,'MomGame',5,-6+2).then((mesh) => {
+    //     MomGame = mesh;
+    //     scene.add(MomGame);
+    // });
+    // gameStuff('./img/Lgg-min.png',3,1.7,'Lgg',5,-4+2).then((mesh) => {
+    //     Lgg = mesh;
+    //     scene.add(Lgg);
+    // });
+    // gameStuff('./img/ClickNaja-min.png',3,1.7,'ClickNaja',5,-2+2).then((mesh) => {
+    //     ClickNaja = mesh;
+    //     scene.add(ClickNaja);
+    // });
+    // gameStuff('./img/GeometricBowling-min.png',3,1.7,'GeometricBowling',5,0+2).then((mesh) => {
+    //     GeometricBowling = mesh;
+    //     scene.add(GeometricBowling);
+    // });
+    // gameStuff('./img/Yingleak-min.png',3,1.7,'Yingleak',5,2+2).then((mesh) => {
+    //     YingLeak = mesh;
+    //     scene.add(YingLeak);
+    // });
 
     //Thai art
     const loaderKrajangYai = new GLTFLoader().setPath( 'models/' );
@@ -235,94 +246,94 @@ function init() {
                 window.open('https://web.facebook.com/cherncheu/' ,'_blank');
             switch (ClickObj.name) {
                 case "ObjGithub":
-                    window.open('https://github.com/abczezeze', '_blank');
+                    window.open('https://github.com/abc3dz', '_blank');
                     break;
                 case "ObjReddit":
-                    window.open('https://www.reddit.com/user/abczezeze', '_blank');
+                    window.open('https://www.reddit.com/user/abc3dz', '_blank');
                    break;
                 case "ObjVercel":
                     console.log(vercelLoad);
                     vercelTurn = true;
                     break;
                 case "ObjITCH":
-                    window.open('https://abczezeze.itch.io/', '_blank');
+                    window.open('https://abc3dz.itch.io/', '_blank');
                     break;
                 case "ObjGameJolt":
-                    window.open('https://gamejolt.com/@abczezeze', '_blank');
+                    window.open('https://gamejolt.com/@abc3dz', '_blank');
                     break;
                 case "ObjINDIEDB":
-                    window.open('https://www.indiedb.com/members/abczezeze', '_blank');
+                    window.open('https://www.indiedb.com/members/abc3dz', '_blank');
                     break;
                 case "ObjPlayStore":
                     window.open('https://play.google.com/store/apps/dev?id=6112214561738871485', '_blank');
                     break;
                 case "ObjSketchfab":
-                    window.open('https://sketchfab.com/ABCzezeze', '_blank');
+                    window.open('https://sketchfab.com/abc3dz', '_blank');
                     break;
                 case "ObjSoundCloud":
-                    window.open('https://soundcloud.com/abczezeze', '_blank');
+                    window.open('https://soundcloud.com/abc3dz', '_blank');
                     break;
                 case "ObjDrive":
                     window.open('https://drive.google.com/drive/folders/14KsuX06G2BkIyWZz2Z6XCIeZBiTAaACA', '_blank');
                     break;
                 case "ObjYT":
-                    window.open('https://www.youtube.com/@abczezeze', '_blank');
+                    window.open('https://www.youtube.com/@abc3dz', '_blank');
                     break;
                 case "ObjFb":
                     window.open('https://web.facebook.com/profile.php?id=61572432430115', '_blank');
                     break;
                 case "ObjX":
-                    window.open('https://x.com/abczezeze', '_blank');
+                    window.open('https://x.com/abc3dz', '_blank');
                     break;
                 case "ObjMastodon":
                     window.open('https://mastodon.gamedev.place/@abczezeze', '_blank');
                     break;
                 case "ObjBluesky":
-                    window.open('https://bsky.app/profile/abczezeze.bsky.social', '_blank');
+                    window.open('https://bsky.app/profile/abc3dz.bsky.social', '_blank');
                     break;
                 case "ObjDeviantart":
                     window.open('https://www.deviantart.com/abc3dz', '_blank');
                     break;
                 case "ObjTumblr":
-                    window.open('https://www.tumblr.com/abczezeze', '_blank');
+                    window.open('https://www.tumblr.com/abc3dz', '_blank');
                     break;
                 case "ObjVimeo":
                     window.open('https://vimeo.com/user84261275', '_blank');
                     break;
-                case "ObjFastwork":
-                    window.open('https://fastwork.co/user/abczezeze', '_blank');
-                    break;
+                // case "ObjFastwork":
+                //     window.open('https://fastwork.co/user/abczezeze', '_blank');
+                //     break;
                 //game stuff
-                case "Tek1":
-                    window.open('https://www.kongregate.com/games/ABC3Dz/tek-game', '_blank');
-                    break;
-                case "Tek2":
-                    window.open('https://abczezeze.itch.io/tekkk', '_blank');
-                    break;
-                case "Tek3":
-                    window.open('https://play.google.com/store/apps/details?id=com.abczezeze.tek', '_blank');
-                    break;
-                case "Tek4":
-                    window.open('https://gamejolt.com/games/tekkk/874075', '_blank');
-                    break;
-                case "Tek5":
-                    window.open('https://abczezeze.github.io/TekGame/', '_blank');
-                    break;
-                case "MomGame":
-                    window.open('https://flowlab.io/game/play/895886', '_blank');
-                    break;
-                case "Lgg":
-                    window.open('https://www.indiedb.com/games/lgg', '_blank');
-                    break;
-                case "ClickNaja":
-                    window.open('https://github.com/abczezeze/ClickNaja', '_blank');
-                    break;
-                case "GeometricBowling":
-                    window.open('https://github.com/abczezeze/GeometricBowling', '_blank');
-                    break;
-                case "Yingleak":
-                    window.open('https://abczezeze.github.io/YingLak/', '_blank');
-                    break;
+                // case "Tek1":
+                //     window.open('https://www.kongregate.com/games/ABC3Dz/tek-game', '_blank');
+                //     break;
+                // case "Tek2":
+                //     window.open('https://abczezeze.itch.io/tekkk', '_blank');
+                //     break;
+                // case "Tek3":
+                //     window.open('https://play.google.com/store/apps/details?id=com.abczezeze.tek', '_blank');
+                //     break;
+                // case "Tek4":
+                //     window.open('https://gamejolt.com/games/tekkk/874075', '_blank');
+                //     break;
+                // case "Tek5":
+                //     window.open('https://abczezeze.github.io/TekGame/', '_blank');
+                //     break;
+                // case "MomGame":
+                //     window.open('https://flowlab.io/game/play/895886', '_blank');
+                //     break;
+                // case "Lgg":
+                //     window.open('https://www.indiedb.com/games/lgg', '_blank');
+                //     break;
+                // case "ClickNaja":
+                //     window.open('https://github.com/abczezeze/ClickNaja', '_blank');
+                //     break;
+                // case "GeometricBowling":
+                //     window.open('https://github.com/abczezeze/GeometricBowling', '_blank');
+                //     break;
+                // case "Yingleak":
+                //     window.open('https://abczezeze.github.io/YingLak/', '_blank');
+                //     break;
                 default:
                     console.log('No action assigned for this object.');
                 }
